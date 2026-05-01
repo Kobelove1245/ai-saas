d9db5b14e9481263b9b3060c0a8b02334c6f3e4d
Jakobi Lovings <Kobe123gsh@gmail.com>
2026-05-01 09:39:02 -0400

    fix voice crash

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/voice', (req, res) => {
-  const voiceResponse = new VoiceResponse();
-  voiceResponse.say('Hi! Sorry we missed your call. We will get back to you soon!');
-  res.type('text/xml');
-  res.send(voiceResponse.toString());
+  try {
+    const VoiceResponse = require('twilio').twiml.VoiceResponse;
+    const voiceResponse = new VoiceResponse();
+
+    voiceResponse.say('Hi! Sorry we missed your call. We will get back to you soon!');
+
+    res.type('text/xml');
+    res.send(voiceResponse.toString());
+  } catch (error) {
+    console.log('VOICE ERROR:', error);
+    res.status(500).send('Error');
+  }
+}); 
 });
 
 app.post('/sms', (req, res) => {
