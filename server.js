de8d4b4e721c622c422e8e0ee314e4334b2d5a42
Jakobi Lovings <Kobe123gsh@gmail.com>
2026-05-01 10:03:26 -0400

    fix twilio voice response

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MessagingResponse, VoiceResponse } = require('twilio');
+const twilio = require('twilio');
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Voice webhook
 app.post('/voice', (req, res) => {
-  try {
-    const VoiceResponse = require('twilio').twiml.VoiceResponse;
-    const voiceResponse = new VoiceResponse();
+  const VoiceResponse = twilio.twiml.VoiceResponse;
+  const response = new VoiceResponse();
 
-    voiceResponse.say('Hi! Sorry we missed your call. We will get back to you soon!');
+  response.say("Hi! Sorry we missed your call. We will get back to you soon!");
 
-    res.type('text/xml');
-    res.send(voiceResponse.toString());
-  } catch (error) {
-    console.log('VOICE ERROR:', error);
-    res.status(500).send('Error');
-  }
-}); 
-});
-
-app.post('/sms', (req, res) => {
-  const smsResponse = new MessagingResponse();
-  smsResponse.message('Hi! Thanks for texting us. We will reply shortly.');
   res.type('text/xml');
-  res.send(smsResponse.toString());
+  res.send(response.toString());
 });
 
+// SMS webhook
+app.post('/sms', (req, res) => {
+  const MessagingResponse = twilio.twiml.MessagingResponse;
+  const response = new MessagingResponse();
+
+  response.message("Hi! Thanks for texting us. We will reply shortly.");
+
+  res.type('text/xml');
+  res.send(response.toString());
+});
+
+// Health check
 app.get('/', (req, res) => {
-  res.send('OK');
+  res.send('Twilio bot is running');
 });
 
+// Start server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
