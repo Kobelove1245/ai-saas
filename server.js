2aebd3873cf0d90a98fef1b1d80e8fc79d008453
Jakobi Lovings <Kobe123gsh@gmail.com>
2026-06-16 13:02:15 -0400

    Extend voicemail recording time

--- server.js
+++ server.js
@@ -20,18 +20,17 @@ app.post('/voice', (req, res) => {
       voice: 'alice',
       language: 'en-US'
     },
-    'Hi. Sorry we missed your call. Please leave your name, phone number, and what you need help with after the beep. We will get back to you soon.'
+    'Hi. Sorry we missed your call. Please leave your name, phone number, and what you need help with after the beep. Take your time. When you are finished, you can hang up.'
   );
 
   voiceResponse.record({
-    maxLength: 60,
+    maxLength: 300,
+    timeout: 10,
     playBeep: true,
     trim: 'trim-silence',
     action: '/recording-complete',
     method: 'POST'
   });
-
-  voiceResponse.hangup();
 
   res.type('text/xml');
   res.send(voiceResponse.toString());
@@ -40,16 +39,6 @@ app.post('/voice', (req, res) => {
 // After voicemail recording is complete
 app.post('/recording-complete', (req, res) => {
   const voiceResponse = new VoiceResponse();
-
-  voiceResponse.say(
-    {
-      voice: 'alice',
-      language: 'en-US'
-    },
-    'Thank you. Your message has been received. Goodbye.'
-  );
-
-  voiceResponse.hangup();
 
   console.log('New voicemail received:');
   console.log('From:', req.body.From);
